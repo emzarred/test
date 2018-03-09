47cd09cdb8f0feb78969db34c2a88241769f3fed
emer zarred <emzarred@gmail.com>
2018-03-09 09:10:59 +0100

    change second dice side

--- MainActivity.java
+++ MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DiceActivity.class);
-                intent.putExtra("max", 20);
+                intent.putExtra("max", 12);
                 startActivity(intent);
             }
         });
